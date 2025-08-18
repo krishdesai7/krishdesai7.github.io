f5194a0ed940e6b52a96e7b781d62d16a6ea9796
Krish Desai <krish@withemissary.com>
2025-08-17 20:31:58 -0700

    Add initial project structure with Python version, main script, and dependencies
    - Created .python-version file specifying Python 3.13
    - Added main.py with a simple greeting function
    - Introduced pyproject.toml for project metadata and dependencies
    - Updated talkmap.py to utilize folium for map generation instead of getorg
    - Added initial HTML and JavaScript files for talkmap visualization
    - Included necessary CSS and JS files for leaflet marker clustering
    - Updated org-locations.js with new talk data
    - Created map.html for displaying the generated map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,82 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
+    "Symmetry Discovery with Deep Learning<br />Thirty-Fifth Annual Conference on Neural Information Processing Systems; Virtual",
+    44.4939936,
+    11.3576917
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
+    "Closed Geodesics on Translation Surfaces<br />Massachusetts Undergraduate Research Conference; Amherst, MA",
+    42.3731948,
+    -72.5198761
+  ],
+  [
+    "Multidimensional Deconvolution with Profiling<br />Thirty-Eighth Annual Conference on Neural Information Processing Systems; Vancouver, Canada",
+    49.2608724,
+    -123.113952
+  ],
+  [
+    "Symmetry Discovery using Machine Learning<br />American Physical Society; Virtual",
+    44.4939936,
+    11.3576917
+  ],
+  [
+    "SymmetryGAN<br />Machine Learning for Jets; Heidelberg, Germany",
+    49.4093582,
+    8.694724
+  ],
+  [
+    "High Energy Physics Seminar<br />Korea Institute for Advanced Study; Seoul, South Korea",
+    37.5666791,
+    126.9782914
+  ],
+  [
+    "CMS Seminar<br />CERN; Geneva, Switzerland",
+    46.2017559,
+    6.1466014
+  ],
+  [
+    "Oblivious Points on Translation Surfaces<br />Young Mathematicians Conference; Columbus, OH",
+    39.9622601,
+    -83.0007065
+  ],
+  [
+    "Deconvolving Detector Effects for Distribution Moments<br />Thirty-Sixth Annual Conference on Neural Information Processing Systems; New Orleans, USA",
+    29.9559145,
+    -90.0730259
+  ],
+  [
+    "Moment extraction using an unfolding protocol without binning<br />PHY-STAT Unfolding Conference; Paris, France",
+    48.8534951,
+    2.3483915
+  ],
+  [
+    "Moment Unfolding using Deep Learning<br />American Physical Society; New York, NY",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Moment Unfolding with Deep Learning<br />Machine Learning for Jets; New Brunswick, NJ",
+    40.4951365,
+    -74.4439254
+  ],
+  [
+    "Symmetry Discovery and Deep Learning<br />Clarifai Perceive Deep Learning AI Conference; Virtual",
+    44.4939936,
+    11.3576917
+  ],
+  [
+    "Neural Posterior Unfolding<br />Thirty-Eighth Annual Conference on Neural Information Processing Systems; Vancouver, Canada",
+    49.2608724,
+    -123.113952
+  ],
+  [
+    "Symmetry Discovery<br />Lawrence Berkeley National Laboratory, Physics Division Seminar; Virtual",
+    44.4939936,
+    11.3576917
+  ],
+  [
+    "Introduction to Symmetry Discovery and Deep Learning<br />Berkeley Compass Lecture; Berkeley, CA",
     37.8708393,
     -122.272863
-  ],
-  [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
-  ],
-  [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
   ]
 ];
