f1faa63bd6e193a51141758ad3b1b06229f9da78
Krish Desai <krishdesai7@gmail.com>
2025-09-15 01:00:48 -0700

    Add package.json and update layout with new scripts
    - Created a new package.json file to manage project dependencies and scripts.
    - Added jQuery and smooth-scroll scripts to the default layout for enhanced functionality.
    - Updated the main JavaScript file by removing unused FitVids initialization to streamline the code.

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -115,9 +115,6 @@ $(document).ready(function () {
   var didResize = false;
   bumpIt();
 
-  // FitVids init
-  fitvids();
-
   // Follow menu drop down
   $(".author__urls-wrapper button").on("click", function () {
     $(".author__urls").fadeToggle("fast", function () { });
